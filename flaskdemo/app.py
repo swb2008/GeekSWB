f7a8ecbd16e9f30ec1cec2eb16458b906edf5975
opollopo <271310065@qq.com>
2021-10-16 14:23:02 +0800

    静态路由示例

--- flaskdemo/app.py
+++ flaskdemo/app.py
@@ -1,11 +1,14 @@
 import flask
+from flask import request
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__,static_url_path='')
 
 
 @app.route('/')
+@app.route('/home')
 def hello_world():
-    return 'Hello World!'
+    user_agent = request.headers.get("User-Agent")
+    return 'Hello World!%s' % user_agent
 
 
 @app.route('/python')
@@ -13,10 +16,13 @@ def py():
     return 'python'
 
 
-@app.route('/user/<name>')
+@app.route('/user/<int:name>')
 def user(name):
+    print(type(name))
     return 'Hello,%s!' % name
 
 
 if __name__ == '__main__':
+    print(app.url_map)
     app.run()
+
