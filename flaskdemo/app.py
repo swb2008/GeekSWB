77b1fbb9cc2b7e5fa8011173842c82ad112ee41a
opollopo <271310065@qq.com>
2021-10-23 14:50:09 +0800

    删除session bug修复

--- flaskdemo/app.py
+++ flaskdemo/app.py
@@ -2,13 +2,13 @@ import flask
 from flask import request, render_template, session
 from flask import make_response
 
-app = flask.Flask(__name__,static_url_path='')
+app = flask.Flask(__name__, static_url_path='')
 app.secret_key = "wefwefwef65"
 
 
 @app.route('/')
 def hello_world():
-    #跳转到登录页
+    # 跳转到登录页
     p = session.get("user_info")
     if not p:
         return render_template("login.html")
@@ -16,40 +16,33 @@ def hello_world():
         return render_template("main.html")
 
 
-
-
 @app.route('/user/<int:name>')
 def user(name):
     print(type(name))
     return 'Hello,%s!' % name
 
-@app.route('/error')
-def err():
-    re = make_response('<h1>系统走丢了</h1>')
-    re.status_code = 666
-    return re
 
-
-@app.route('/info',methods=["post"])
+@app.route('/info', methods=["post"])
 def info():
     name = request.form.get("username")
     password = request.form.get("password")
     d = {}
-    with open("userinfo","r") as f:
+    with open("userinfo", "r") as f:
         d = eval(f.read())
     if d.__contains__(name):
         # session是一个特殊的字典 {"":"","":""}
         session["user_info"] = name
-        return render_template("main.html",msg2=name)
+        return render_template("main.html", msg2=name)
     else:
-        return render_template("login.html",msg='用户名或密码错误')
+        return render_template("login.html", msg='用户名或密码错误')
+
+
 @app.route('/logout')
 def logout():
-    del session
+    del session["user_info"]
     return render_template("login.html")
+
 
 if __name__ == '__main__':
     print(app.url_map)
     app.run()
-
-
