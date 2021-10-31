a817ff5fe33729d0a2ac156066d515ab9ed1989f
opollopo <271310065@qq.com>
2021-10-31 15:27:54 +0800

    页面bug修复

--- flaskdemo/app.py
+++ flaskdemo/app.py
@@ -1,3 +1,5 @@
+import os
+
 import flask
 from flask import request, render_template, session
 from flask import make_response
@@ -57,16 +59,32 @@ def creat_room():
     target_num = str(random.randint(1, 200))
     r_t_n = room_num + " " + target_num
     with open("room_tag.txt", "a+") as f:
-        f.write(r_t_n + "/n")
+        f.write(r_t_n + "\n")
     return "创建的房间号为：%s,生成目标数字是%s" % (room_num, target_num)
 
-@app.route("/join",methods=["post"])
+
+@app.route("/join", methods=["post"])
 def online_public():
-    """"加入房间"""
-    #获取房间号
-    room_number=request.form.get("room_num")
-    #判断文件是否存在
-    res=os.path.exists("room_tag.txt")
+    """加入房间"""
+    # 获取房间号
+    room_number = request.form.get("room_num")
+    # 判断文件是否存在
+    res = os.path.exists("room_tag.txt")
+    if res:
+        # 读取room_tag.txt这个文件
+        with open("room_tag.txt", "r") as f:
+            while 1:
+                room_tag_data = f.readline()
+                if room_tag_data:
+                    x, y = room_tag_data.split(" ")
+                    y = y.replace('\n', '')
+                    if room_number == x:
+                        return render_template("guess.html", v_y=y)
+                    else:
+                        return "房间号不存在"
+    else:
+        return "房间号不存在，请创建房间号"
+
 
 if __name__ == '__main__':
     print(app.url_map)
