a6eee48966779ac8d0b9ca283763d0a018435c4b
Administrator <271310065@qq.com>
2021-11-27 14:40:00 +0800

    插入数据库

--- articlesCenter/swbApp2.py
+++ articlesCenter/swbApp2.py
@@ -6,11 +6,13 @@ app = flask.Flask(__name__, static_url_path='')
 app.debug = True
 
 cu = None
+cx = None
 
 
 @app.before_first_request
 def con():
     global cu
+    global cx
     # 连接数据库,创建连接对象
     cx = sqlite3.connect("test.db")
     # 创建游标
@@ -20,6 +22,16 @@ def con():
 @app.route('/')
 def hello_world():
     return render_template("main.html")
+
+
+@app.route('/create', methods=["post"])
+def create():
+    t = request.form.get('title')
+    c = request.form.get('content')
+    cu.execute("insert into articles ( title, content, author) VALUES ('%s','%s','%s')" % (t, c, 'swb'))
+    cx.commit()
+
+    return render_template("main.html", msg='发布成功')
 
 
 if __name__ == '__main__':
